a18aada3470b3ede4e11c4870d9c2177288b881e
hernandezcu <hernandezcu@jbu.edu>
2022-02-19 10:58:14 -0600

    Added Home Screen

--- WK 3/main.py
+++ WK 3/main.py
@@ -11,8 +11,36 @@ x = import_data()
 @app.get("/", response_class=HTMLResponse)
 def read_root():
     return """
-    <h1>Welcome</h1>
-    <body>Docs can be found at /docs</body>
+    <html><head><title>JBU Advanced CSA</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1 {font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+.bgimg {
+  background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/800px-A_black_image.jpg');
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+</style>
+</head><body>
+
+<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+  <div class="w3-display-topleft w3-padding-large w3-xlarge">
+    ADVANCED CSA FASTAPI EXAMPLE
+  </div>
+  <div class="w3-display-middle">
+    <h1 class="w3-jumbo w3-animate-top">Work in Progress</h1>
+    <hr class="w3-border-grey" style="margin:auto;width:40%">
+    <p class="w3-large w3-center">add <a href="http://localhost:8000/docs" target="_blank">/docs</a> to your url to view endpoints</p>
+  </div>
+</div>
+
+
+</body></html>
     """
 
 
